38ae6681ea09bda5c4c83b575aa6dfe6adda459f
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-05-25 18:05:40 -0500

    check me.shoeCount after adding one shoe

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -43,6 +43,7 @@ console.log(me.favThreeFoods[2]);
 */
 console.log(me.shoeCount);
 me.shoeCount += 1;
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
