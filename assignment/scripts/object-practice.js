28cc82973cce7d94d710dcb4889763ecdaad00cb
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-05-25 17:38:39 -0500

    change fullName to properly space the name

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -23,7 +23,7 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-let fullName = me.firstName + me.lastName;
+let fullName = `${me.firstName} ${me.lastName}`;
 
 console.log(fullName);
 
