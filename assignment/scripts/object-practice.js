3b626b624a5eb92307a402849d3350c1b7169dda
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-06-04 14:46:09 -0500

    Add comments

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,13 +9,14 @@ console.log('***** Object Practice *****');
   - Also give it an array 'favThreeFoods'
 */
 const me = {
+	//! Create the me onbject
 	firstName: 'Stephon',
 	lastName: 'Buckhanan',
 	hasSiblings: true,
 	shoeCount: 3,
 	favThreeFoods: ['Pizza', 'Tacos', 'Fries']
 };
-console.log('A little about me:', me);
+console.log('A little about me:', me); //! Will console log all the properties and their values in the me object
 
 /* 2. Accessing object properties.
   - Create a variable called fullName 
@@ -23,17 +24,17 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-let fullName = `${me.firstName} ${me.lastName}`;
+let fullName = `${me.firstName} ${me.lastName}`; //! Declare full name variable. Combines me.firstName and me.lastName
 
-console.log(fullName);
+console.log(fullName); //! Console logs the fullName variable. Should show Stephon Buckhanan.
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
 
-console.log(me.favThreeFoods[0]);
-console.log(me.favThreeFoods[2]);
+console.log(me.favThreeFoods[0]); //! Should show pizza
+console.log(me.favThreeFoods[2]); //! Should show Fries
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -41,9 +42,9 @@ console.log(me.favThreeFoods[2]);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
-console.log(me.shoeCount);
-me.shoeCount += 1;
-console.log(me.shoeCount);
+console.log(me.shoeCount); //! Should show 3
+me.shoeCount += 1; //! Add 1 shoe to the shoeCount
+console.log(me.shoeCount); //! shoeCount should now show 4
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -52,6 +53,6 @@ console.log(me.shoeCount);
   - Console.log your object
 */
 
-me.favoriteColor = 'Blue';
+me.favoriteColor = 'Blue'; //! Add a new property to the me object with a value of Blue
 
-console.log(me.favoriteColor);
+console.log(me.favoriteColor); //! Should show Blue
